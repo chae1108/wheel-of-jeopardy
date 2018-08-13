b1d1b0420cca3dd2696a5ebd3204d3308dd49594
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-13 11:13:54 -0400

    Update categorySelect.py

--- ui/categorySelect.py
+++ ui/categorySelect.py
@@ -1,44 +1,71 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui/categorySelect.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QAbstractItemView
+import os
 
 class Ui_categorySelect(object):
+
+    # I've changed the GUIs around a bit.  They are no londer resizable.  I've added a WOJ logo.  The StandardItemModel
+    # adds the categories to select from.
     def setupUi(self, categorySelect):
         categorySelect.setObjectName("categorySelect")
-        categorySelect.resize(947, 681)
-        self.title = QtWidgets.QTextEdit(categorySelect)
-        self.title.setGeometry(QtCore.QRect(290, 20, 401, 51))
-        self.title.setObjectName("title")
+        categorySelect.setFixedSize(1000, 600)
+        self.logo = QtWidgets.QLabel(categorySelect)
+        self.logo.setGeometry(QtCore.QRect(75, 15, 1000, 100))
+        self.logo.setPixmap(QtGui.QPixmap('WOJlogo.png'))
+        self.logo.setObjectName("logo")
+
         self.availableList = QtWidgets.QListView(categorySelect)
-        self.availableList.setGeometry(QtCore.QRect(80, 80, 341, 461))
+        self.availableList.setGeometry(QtCore.QRect(150, 130, 300, 400))
         self.availableList.setObjectName("availableList")
+        self.availableList.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        categories = self.getCategoryNames()
+
+        self.availableModel = QtGui.QStandardItemModel()
+        self.availableList.setModel(self.availableModel)
+        for category in categories:
+            item = QtGui.QStandardItem(category)
+            self.availableModel.appendRow(item)
+
+        self.chosenList = QtWidgets.QListView(categorySelect)
+        self.chosenList.setGeometry(QtCore.QRect(550, 130, 300, 400))
+        self.chosenList.setObjectName("chosenList")
+        self.chosenModel = QtGui.QStandardItemModel()
+        self.chosenList.setModel(self.chosenModel)
+
         self.startGame = QtWidgets.QPushButton(categorySelect)
-        self.startGame.setGeometry(QtCore.QRect(180, 560, 241, 81))
+        self.startGame.setGeometry(QtCore.QRect(150, 530, 300, 70))
         self.startGame.setObjectName("startGame")
         self.cancel = QtWidgets.QPushButton(categorySelect)
-        self.cancel.setGeometry(QtCore.QRect(460, 560, 241, 81))
+        self.cancel.setGeometry(QtCore.QRect(550, 530, 300, 70))
         self.cancel.setObjectName("cancel")
-        self.chosenList = QtWidgets.QListView(categorySelect)
-        self.chosenList.setGeometry(QtCore.QRect(480, 80, 341, 461))
-        self.chosenList.setObjectName("chosenList")
+
+        self.moveToLeftColumn = QtWidgets.QPushButton(categorySelect)
+        self.moveToLeftColumn.setGeometry(QtCore.QRect(475, 300, 50, 50))
+        self.moveToLeftColumn.setObjectName("moveToLeft")
+
+        self.moveToRightColumn = QtWidgets.QPushButton(categorySelect)
+        self.moveToRightColumn.setGeometry(QtCore.QRect(475, 250, 50, 50))
+        self.moveToRightColumn.setObjectName("moveToRight")
 
         self.retranslateUi(categorySelect)
         QtCore.QMetaObject.connectSlotsByName(categorySelect)
 
     def retranslateUi(self, categorySelect):
         _translate = QtCore.QCoreApplication.translate
-        categorySelect.setWindowTitle(_translate("categorySelect", "Form"))
-        self.title.setHtml(_translate("categorySelect", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">Wheel of Jeopardy</span></p></body></html>"))
+        categorySelect.setWindowTitle(_translate("categorySelect", "Wheel of Jeopardy"))
         self.startGame.setText(_translate("categorySelect", "Start Game"))
         self.cancel.setText(_translate("categorySelect", "Cancel"))
+        self.moveToLeftColumn.setText(_translate("categorySelect", "<<"))
+        self.moveToRightColumn.setText(_translate("categorySelect", ">>"))
 
+    # Removed the .csv extension from the filename
+    def getCategoryNames(self):
+        categoryNames = []
+        fileNames = os.listdir("./db")
+        for file in fileNames:
+            if file[-4:] == ".csv":
+                file = file[:-4]
+                categoryNames.append(file)
+
+        return categoryNames
