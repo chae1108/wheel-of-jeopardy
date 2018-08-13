8a21ffcd4502c7b91bb6c97dddaae4dab0617742
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-13 11:13:26 -0400

    Update categoryMenu.py

--- ui/categoryMenu.py
+++ ui/categoryMenu.py
@@ -1,20 +1,15 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui/categoryMenu.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_categoryMenu(object):
+
+    # I've changed the GUIs around a bit.  They are no londer resizable.  I've added a WOJ logo
     def setupUi(self, categoryMenu):
         categoryMenu.setObjectName("categoryMenu")
-        categoryMenu.resize(1000, 700)
-        self.title = QtWidgets.QTextEdit(categoryMenu)
-        self.title.setGeometry(QtCore.QRect(300, 40, 401, 51))
-        self.title.setObjectName("title")
+        categoryMenu.setFixedSize(1000, 600)
+        self.logo = QtWidgets.QLabel(categoryMenu)
+        self.logo.setGeometry(QtCore.QRect(75, 40, 1000, 200))
+        self.logo.setPixmap(QtGui.QPixmap('WOJlogo.png'))
+        self.logo.setObjectName("logo")
         self.categoryEdit = QtWidgets.QPushButton(categoryMenu)
         self.categoryEdit.setGeometry(QtCore.QRect(360, 420, 311, 71))
         self.categoryEdit.setObjectName("categoryEdit")
@@ -33,14 +28,8 @@ class Ui_categoryMenu(object):
 
     def retranslateUi(self, categoryMenu):
         _translate = QtCore.QCoreApplication.translate
-        categoryMenu.setWindowTitle(_translate("categoryMenu", "Form"))
-        self.title.setHtml(_translate("categoryMenu", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">Wheel of Jeopardy</span></p></body></html>"))
+        categoryMenu.setWindowTitle(_translate("categoryMenu", "Wheel of Jeopardy"))
         self.categoryEdit.setText(_translate("categoryMenu", "Edit Category"))
         self.back.setText(_translate("categoryMenu", "Back"))
         self.categoryDelete.setText(_translate("categoryMenu", "Delete Category"))
         self.categoryAdd.setText(_translate("categoryMenu", "Add Category"))
-
