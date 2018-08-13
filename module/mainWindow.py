e92e9f3d857388698978ab4256137c7b016daff2
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-13 11:10:40 -0400

    Update mainWindow.py

--- module/mainWindow.py
+++ module/mainWindow.py
@@ -1,21 +1,33 @@
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from ui.main import Ui_main
 from module.gameWindow import GameWindow
 from module.categoryMenuWindow import CategoryMenuWindow
+from module.categorySelectWindow import CategorySelectWindow
 
 class MainWindow(QtWidgets.QMainWindow, Ui_main):
+
+    # Added a close window button
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.newGame.clicked.connect(self.goToGame)
+        self.newGame.clicked.connect(self.goToSelectCategory)
         self.categories.clicked.connect(self.goToCategoryMenu)
+        self.quit.clicked.connect(self.closeWindow)
 
-    def goToGame(self):
+    def goToSelectCategory(self):
         self.close()
-        self.goToGame = GameWindow(parent=self)
-        self.goToGame.show()
+        self.goToSelectCategory = CategorySelectWindow(parent=self)
+        self.goToSelectCategory.show()
 
     def goToCategoryMenu(self):
         self.close()
         self.goToCategoryMenu = CategoryMenuWindow(parent=self)
         self.goToCategoryMenu.show()
+
+    def closeWindow(self):
+        option = QMessageBox.question(self, "Quit Game", "Are you sure you want to quit?", QMessageBox.Yes | QMessageBox.No)
+        if(option == QMessageBox.Yes):
+            self.close()
+        else:
+            pass
