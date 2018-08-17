e05427c0adc4b23e031d3b68424bf70d0d0a55df
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-17 14:14:12 -0400

    Update categoryDelete.py

--- ui/categoryDelete.py
+++ ui/categoryDelete.py
@@ -1,41 +1,60 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui/categoryDelete.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+import os
 
 class Ui_categoryDelete(object):
     def setupUi(self, categoryDelete):
         categoryDelete.setObjectName("categoryDelete")
-        categoryDelete.resize(947, 681)
-        self.title = QtWidgets.QTextEdit(categoryDelete)
-        self.title.setGeometry(QtCore.QRect(290, 20, 401, 51))
-        self.title.setObjectName("title")
+        categoryDelete.setFixedSize(950, 665)
+        self.logo = QtWidgets.QLabel(categoryDelete)
+        self.logo.setGeometry(QtCore.QRect(50, 15, 1000, 100))
+        self.logo.setPixmap(QtGui.QPixmap('WOJlogo.png'))
+        self.logo.setObjectName("logo")
+        self.list = QtWidgets.QListWidget(categoryDelete)
+        self.list.setGeometry(QtCore.QRect(190, 120, 580, 460))
+        self.list.setObjectName("list")
         self.remove = QtWidgets.QPushButton(categoryDelete)
-        self.remove.setGeometry(QtCore.QRect(180, 560, 241, 81))
+        self.remove.setGeometry(QtCore.QRect(240, 600, 211, 51))
         self.remove.setObjectName("remove")
         self.cancel = QtWidgets.QPushButton(categoryDelete)
-        self.cancel.setGeometry(QtCore.QRect(460, 560, 241, 81))
+        self.cancel.setGeometry(QtCore.QRect(510, 600, 211, 51))
         self.cancel.setObjectName("cancel")
-        self.list = QtWidgets.QListWidget(categoryDelete)
-        self.list.setGeometry(QtCore.QRect(200, 110, 551, 421))
-        self.list.setObjectName("list")
+
+        categories = self.getCategoryNames()
+
+        for category in categories:
+            item = QtWidgets.QListWidgetItem(category)
+            self.list.addItem(item.text())
 
         self.retranslateUi(categoryDelete)
         QtCore.QMetaObject.connectSlotsByName(categoryDelete)
 
     def retranslateUi(self, categoryDelete):
         _translate = QtCore.QCoreApplication.translate
-        categoryDelete.setWindowTitle(_translate("categoryDelete", "Form"))
-        self.title.setHtml(_translate("categoryDelete", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">Wheel of Jeopardy</span></p></body></html>"))
+        categoryDelete.setWindowTitle(_translate("categoryDelete", "Delete Category"))
         self.remove.setText(_translate("categoryDelete", "Delete"))
         self.cancel.setText(_translate("categoryDelete", "Cancel"))
 
+    def getCategoryNames(self):
+        categoryNames = []
+        fileNames = os.listdir("./db")
+        for file in fileNames:
+            if file[-4:] == ".csv":
+                file = file[:-4]
+                categoryNames.append(file)
+
+        return categoryNames
+
+    def selectCategory(self):
+        try:
+            category = self.list.currentItem().text()
+            item = self.list.currentRow()
+            ci = [category, item]
+        except:
+            message = QMessageBox.question(self, "Error",
+                                           "Please select a category.",
+                                           QMessageBox.Ok)
+            if (message == QMessageBox.Ok):
+                pass
+        else:
+            return ci
