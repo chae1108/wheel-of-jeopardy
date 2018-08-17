6101e286de1d9c16ba9a89cc6b832255c6c9314d
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-17 17:40:06 -0400

    Update gameChoiceWindow.py

--- module/gameChoiceWindow.py
+++ module/gameChoiceWindow.py
@@ -1,8 +1,14 @@
 from PyQt5 import QtGui, QtWidgets
-from ui.game import Ui_game
 from ui.gameChoice import Ui_gameChoice
+from module.gameAnswerWindow import GameAnswerWindow
+from classes.Game import Game
 
 class GameChoiceWindow(QtWidgets.QMainWindow, Ui_gameChoice):
-    def __init__(self, parent):
+    def __init__(self, game, parent):
         super(GameChoiceWindow, self).__init__(parent)
-        self.setupUi(self)
+        self.setupUi(self, game)
+        self.select.clicked.connect(self.selectToPlay)
+
+    def selectToPlay(self):
+        self.close()
+        categoryChoice = self.selectCategory()
