3aec6f4c72417d57a6ca94c0f1981795069bd18b
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-17 17:38:36 -0400

    Update gameWindow.py

--- module/gameWindow.py
+++ module/gameWindow.py
@@ -1,51 +1,70 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from ui.game import Ui_game
 from classes.Game import Game
-import module.gameChoiceWindow
-
 from module.gameChoiceWindow import GameChoiceWindow
+from module.gameAnswerWindow import GameAnswerWindow
 from ui.gameChoice import Ui_gameChoice
+from ui.gameAnswer import Ui_gameAnswer
+from PyQt5.QtWidgets import QMessageBox
+import os
+import csv
 
 class GameWindow(QtWidgets.QMainWindow, Ui_game):
 
-    def __init__(self, parent):
+    def __init__(self, categoriesSelected, parent):
         super(GameWindow, self).__init__(parent)
-        self.setupUi(self)
+        self.categories = self.getCategories(categoriesSelected)
+        self.setupUi(self, self.categories)
+        self.game = Game(self.categories)
+        self.spin = ""
 
-        # These are test categories.  Once we can select the categories, this will be replaced with those.
-        cat0 = ["cat0", "c0q0", "c0a0", "c0q1", "c0a1", "c0q2", "c0a2", "c0q3", "c0a3", "c0q4", "c0a5"]
-        cat1 = ["cat1", "c1q0", "c1a0", "c1q1", "c1a1", "c1q2", "c1a2", "c1q3", "c1a3", "c1q4", "c1a4"]
-        cat2 = ["cat2", "c2q0", "c2a0", "c2q1", "c2a1", "c2q2", "c2a2", "c2q3", "c2a3", "c2q4", "c2a4"]
-        cat3 = ["cat3", "c3q0", "c3a0", "c3q1", "c3a1", "c3q2", "c3a2", "c3q3", "c3a3", "c3q4", "c3a4"]
-        cat4 = ["cat4", "c4q0", "c4a0", "c4q1", "c4a1", "c4q2", "c4a2", "c4q3", "c4a3", "c4q4", "c4a4"]
-        cat5 = ["cat5", "c5q0", "c5a0", "c5q1", "c5a1", "c5q2", "c5a2", "c5q3", "c5a3", "c5q4", "c5a4"]
-        categories = [cat0, cat1, cat2, cat3, cat4, cat5]
-        self.game = Game(categories)
         self.updateUI()
 
-        self.runGame()
+        self.spinner.clicked.connect(self.spinWheel)
         self.back.clicked.connect(self.close)
         self.back.clicked.connect(parent.show)
 
-    def runGame(self):
-        self.spinner.clicked.connect(self.spinWheel)
+    def getCategories(self, categoriesSelected):
+        categories = []
+        for cat in categoriesSelected:
+            category = self.__getCategory(cat)
+            categories.append(category)
+
+        return categories
+
+    def __getCategory(self, cat):
+        filename = os.path.join('db', cat + '.csv')
+        try:
+            with open(filename, "r") as csvfile:
+                rowReader = csv.reader(csvfile, delimiter='|')
+                for row in rowReader:
+                    return row
+        except:
+            message = QMessageBox.question(self, "Error",
+                                           "File does not exist",
+                                           QMessageBox.Ok)
+            if (message == QMessageBox.Ok):
+                self.close()
+            else:
+                pass
 
     def spinWheel(self):
-        spin = self.game.spinWheel()
+        self.spin = self.game.spinWheel()
 
-        # Need to select a category
-        if spin == 7 or spin == 9:
-            self.goToGameChoice = GameChoiceWindow(parent=self)
+        # Opens the Game Choice but needs to get the selected category
+        if self.spin == 7 or self.spin == 9:
+            self.goToGameChoice = GameChoiceWindow(self.game, parent=self)
             self.goToGameChoice.show()
-            #spin = categoryChoice
+            self.spin = 1 #LOSE A TURN FOR NOW
 
-        self.game.playTurn(spin)
+        # Once it has a category or if the category is spun, brings up the Game Answer window.  Should determine if
+        # player answered correctly and adject accordingly.
+        if self.spin == 0 or self.spin == 2 or self.spin == 4 or self.spin == 6 or self.spin == 8 or self.spin == 10:
+            data = self.game.playCategory(self.spin)
+            if not(data[0] == ""):
+                self.goToGameAnswer = GameAnswerWindow(data, parent=self)
+                self.goToGameAnswer.show()
+        else:
+            self.game.playTurn(self.spin)
+
         self.updateUI()
-
-    def updateUI(self):
-        _translate = QtCore.QCoreApplication.translate
-        self.round.setText(_translate("game", "Round: " + str(self.game.getRound())))
-        self.spins.setText(_translate("game", "Spins: " + str(self.game.getSpins())))
-        self.contestant1.setText(_translate("game", "Contestant 1: $" + str(self.game.getPlayer(0).getTotalScore())))
-        self.contestant2.setText(_translate("game", "Contestant 2: $" + str(self.game.getPlayer(1).getTotalScore())))
-        self.contestant3.setText(_translate("game", "Contestant 3: $" + str(self.game.getPlayer(2).getTotalScore())))
