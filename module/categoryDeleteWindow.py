b4befd646b72d631f1f6240700b3243740c46cfe
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-17 14:10:10 -0400

    Update categoryDeleteWindow.py

--- module/categoryDeleteWindow.py
+++ module/categoryDeleteWindow.py
@@ -1,39 +1,26 @@
 from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from ui.categoryDelete import Ui_categoryDelete
 import os
-import csv
 
 class CategoryDeleteWindow(QtWidgets.QMainWindow, Ui_categoryDelete):
     def __init__(self, parent):
         super(CategoryDeleteWindow, self).__init__(parent)
         self.setupUi(self)
-        for file in os.listdir(os.path.join(os.getcwd(),'db')):
-            with open(os.path.join(os.getcwd(),'db',file)) as csvfile:
-                ader = csv.reader(csvfile, delimiter='|')
-                for row in ader:
-                    self.list.addItem(row[0])        
+        self.remove.clicked.connect(self.goToDelete)
         self.cancel.clicked.connect(self.close)
         self.cancel.clicked.connect(parent.show)
-        self.remove.clicked.connect(self.deleteCategory)
 
-    def deleteCategory(self):
-        try:
-            self.message = QtWidgets.QMessageBox(parent=self)
-            for file in os.listdir(os.path.join(os.getcwd(),'db')):
-                removal = False
-                with open(os.path.join(os.getcwd(),'db',file)) as csvfile:
-                    ader = csv.reader(csvfile, delimiter='|')
-                    for row in ader:
-                        if row[0] == self.list.selectedItems()[0].text():
-                            removal = True
-                if removal == True:
-                    os.remove(os.path.join(os.getcwd(),'db',file))
-        except:
-            self.message.setWindowTitle("Category Status")
-            self.message.setText("The category has not been deleted.")
-            self.message.show()
-        else:    
-            self.message.setWindowTitle("Category Status")
-            self.message.setText("The category has been deleted.")
-            self.message.show()   
+    def goToDelete(self):
+        ci = self.selectCategory()
+        filename = os.path.join('db', ci[0] + '.csv')
+        if os.path.exists(filename):
 
+            option = QMessageBox.question(self, "Are you sure?",
+                                               "Are you sure you want to delete '" + ci[0] + "'?",
+                                               QMessageBox.Yes | QMessageBox.No)
+            if option == QMessageBox.Yes:
+                os.remove(filename)
+                self.list.takeItem(ci[1])
+            else:
+                pass
