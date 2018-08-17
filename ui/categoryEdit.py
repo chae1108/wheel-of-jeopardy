9b909f43a735ce90d4fead38eaa0936e5c978aaf
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-17 14:14:52 -0400

    Update categoryEdit.py

--- ui/categoryEdit.py
+++ ui/categoryEdit.py
@@ -1,41 +1,61 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui/categoryEdit.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+import os
 
 class Ui_categoryEdit(object):
+
     def setupUi(self, categoryEdit):
         categoryEdit.setObjectName("categoryEdit")
-        categoryEdit.resize(947, 681)
-        self.title = QtWidgets.QTextEdit(categoryEdit)
-        self.title.setGeometry(QtCore.QRect(290, 20, 401, 51))
-        self.title.setObjectName("title")
+        categoryEdit.resize(950, 665)
+        self.logo = QtWidgets.QLabel(categoryEdit)
+        self.logo.setGeometry(QtCore.QRect(50, 15, 1000, 100))
+        self.logo.setPixmap(QtGui.QPixmap('WOJlogo.png'))
+        self.logo.setObjectName("logo")
+
+        self.selectList = QtWidgets.QListWidget(categoryEdit)
+        self.selectList.setGeometry(QtCore.QRect(190, 120, 580, 460))
+        self.selectList.setObjectName("list")
+
+        categories = self.getCategoryNames()
+
+        for category in categories:
+            item = QtWidgets.QListWidgetItem(category)
+            self.selectList.addItem(item.text())
+
         self.select = QtWidgets.QPushButton(categoryEdit)
-        self.select.setGeometry(QtCore.QRect(180, 560, 241, 81))
+        self.select.setGeometry(QtCore.QRect(240, 600, 211, 51))
         self.select.setObjectName("select")
         self.cancel = QtWidgets.QPushButton(categoryEdit)
-        self.cancel.setGeometry(QtCore.QRect(460, 560, 241, 81))
+        self.cancel.setGeometry(QtCore.QRect(510, 600, 211, 51))
         self.cancel.setObjectName("cancel")
-        self.list = QtWidgets.QListWidget(categoryEdit)
-        self.list.setGeometry(QtCore.QRect(150, 100, 641, 441))
-        self.list.setObjectName("list")
 
         self.retranslateUi(categoryEdit)
         QtCore.QMetaObject.connectSlotsByName(categoryEdit)
 
     def retranslateUi(self, categoryEdit):
         _translate = QtCore.QCoreApplication.translate
-        categoryEdit.setWindowTitle(_translate("categoryEdit", "Form"))
-        self.title.setHtml(_translate("categoryEdit", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:14pt; font-weight:600;\">Wheel of Jeopardy</span></p></body></html>"))
+        categoryEdit.setWindowTitle(_translate("categoryEdit", "Select Category"))
         self.select.setText(_translate("categoryEdit", "Select"))
         self.cancel.setText(_translate("categoryEdit", "Cancel"))
 
+    def getCategoryNames(self):
+        categoryNames = []
+        fileNames = os.listdir("./db")
+        for file in fileNames:
+            if file[-4:] == ".csv":
+                file = file[:-4]
+                categoryNames.append(file)
+
+        return categoryNames
+
+    def selectCategory(self):
+        try:
+            category = self.selectList.currentItem().text()
+        except:
+            message = QMessageBox.question(self, "Error",
+                                           "Please select a category.",
+                                           QMessageBox.Ok)
+            if (message == QMessageBox.Ok):
+                pass
+        else:
+            return category
