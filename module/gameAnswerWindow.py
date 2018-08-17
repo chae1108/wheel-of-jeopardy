7ffe56d67839638243ef72bd827f27fae7058a39
tvandermosten <41882031+tvandermosten@users.noreply.github.com>
2018-08-17 17:41:43 -0400

    Update gameAnswerWindow.py

--- module/gameAnswerWindow.py
+++ module/gameAnswerWindow.py
@@ -1,7 +1,20 @@
 from PyQt5 import QtGui, QtWidgets
 from ui.game import Ui_game
+from ui.gameAnswer import Ui_gameAnswer
 
 class GameAnswerWindow(QtWidgets.QMainWindow, Ui_gameAnswer):
-    def __init__(self, parent):
+    def __init__(self, data, parent):
         super(GameAnswerWindow, self).__init__(parent)
-        self.setupUi(self)
+        self.setupUi(self, data)
+        self.showAnswer.clicked.connect(self.goToAnswer)
+        self.right.clicked.connect(self.rightAnswer)
+        self.wrong.clicked.connect(self.wrongAnswer)
+
+    def goToAnswer(self):
+        self.updateUI()
+
+    def rightAnswer(self):
+        self.close()
+
+    def wrongAnswer(self):
+        self.close()
